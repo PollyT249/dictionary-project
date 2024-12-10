ebc9001ab189503a5aef71cffb83170d1834831e
PollyT249 <polinatretiakova249@gmail.com>
2024-12-10 20:45:11 +0200

    Rendered the word synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
+  let example = props.meaning.example;
+
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
       <p>
-        <em>{props.meaning.example}</em>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
       </p>
+      <p className="Meaning-example">
+        <em>{example ? `Example: ${example}` : null}</em>
+      </p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
